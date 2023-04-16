c2049d2fec7b4d88ece4c4275278eebefe666ace
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-04-16 18:45:15 -0300

    efeito hamburger adicionado

--- app.js
+++ app.js
@@ -8,36 +8,25 @@ const escape = document.querySelector(".sobreMimBotaoEscape");
 const iconePrincipal = document.querySelector(".icon-logo");
 const conteudoTitulo = document.querySelector(".conteudo-titulo");
 const conteudoApresentacao = document.querySelector(".conteudo-apresentacao");
-// tentando criar novo hamburger animado
-//teste
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 
-// acima criando de teste
-// click eventos para abrir a aba
-menuBtn.addEventListener("click", sobreMimOPC);
-menuBtn.addEventListener("click", fecharAba);
+menuBtn.addEventListener("click", abrindoAba);
 
 // função abrindo aba
-function sobreMimOPC() {
-  sobreMimProjetos.classList.remove("sobreMimProjetos1");
-  sobreMimProjetos2.classList.remove("sobreMimProjetos2");
-  sobreMimProjetos3.classList.remove("sobreMimProjetos3");
-  escape.classList.remove("sobreMimBotaoEscape");
-  iconePrincipal.classList.add("icon-logo-aba-aberta");
-  hamburger.classList.add("esconder");
-}
-
-//função fechando a aba
-function fecharAba() {
-  if (menuOpen === true) {
-    escape.classList.add("sobreMimBotaoEscape");
+function abrindoAba() {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    sobreMimProjetos.classList.remove("sobreMimProjetos1");
+    sobreMimProjetos2.classList.remove("sobreMimProjetos2");
+    sobreMimProjetos3.classList.remove("sobreMimProjetos3");
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
     sobreMimProjetos.classList.add("sobreMimProjetos1");
     sobreMimProjetos2.classList.add("sobreMimProjetos2");
     sobreMimProjetos3.classList.add("sobreMimProjetos3");
-    hamburger.style.opacity = "2";
-    hamburger.classList.remove("esconder");
-    iconePrincipal.classList.remove("icon-logo-aba-aberta");
   }
 }
 
@@ -54,17 +43,3 @@ function typeWriter(elemento) {
 }
 
 typeWriter(conteudoTitulo);
-
-// efeito rolagem
-
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
-});
-
-console.log(menuOpen);
